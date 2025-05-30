adf8fe743e4fc435883247022d30af54c34fb420
Hamza NADIFI <nadifihamza07@gmail.com>
2025-05-30 07:04:15 +0200

    Géneration du document de domiciliation de travail finie

--- src/type/domiciliationDeTravail/DomiciliationDeTravail.jsx
+++ src/type/domiciliationDeTravail/DomiciliationDeTravail.jsx
@@ -38,7 +38,7 @@ function DomiciliationDeTravail() {
       generatePDF()
     }
   
-    const civilite = formData.genre === "Femme" ? "Madame" : "Monsieur"
+    const civilite = formData.genre === "Femme" ? "Mme" : "Mr"
 
     const generatePDF = () => {
       const doc = new jsPDF()
@@ -48,37 +48,59 @@ function DomiciliationDeTravail() {
       img.onload = function () {
         doc.addImage(img, 'PNG', 10, 10, 50, 20)
     
-        doc.setFont("Times" , "bolditalic")
-        doc.setFontSize(20)
-        doc.text("ENGAGEMENT DE DOMICILIATION IRRÉVOCABLE DE SALAIRE", 105, 60, { align: "center" })
-        doc.setLineWidth(0.5)
-        doc.line(57, 62, 153, 62) 
+        doc.setFont("Times" , "bold")
         doc.setFontSize(16)
+        doc.text("ENGAGEMENT DE DOMICILIATION IRRÉVOCABLE DE SALAIRE", 105, 40, { align: "center" })
+        doc.setLineWidth(0.5)
+        doc.line(17, 42, 195, 42) 
+        doc.setFontSize(12)
         doc.setFont("Times" , "normal")
-        const content = 
+        const paragraph1 = 
           "Nous, soussignés, la société LAVAL ACADEMY PRIVE représentée par Mr AHMITO MOHAMMED en sa qualité de GERANT habilité à l'effet de la présente attestations que " +
           civilite + " " + formData.nom.toUpperCase() + " " + formData.prenom.toUpperCase() + ", titulaire de la carte d’identité nationale n° " +
           formData.cin + ", exerce au sein de notre établissement en qualité de: \n\n" +
-          "•  " + formData.poste + "\n\n" +
+          "   •  " + formData.poste + "\n\n" +
           "Et ce, depuis le " + formatDateFrench(formData.dateEmbauche) + ".\n\n" +
-          civilite + " " + formData.nom.toUpperCase() + " " + formData.prenom.toUpperCase() + "perçoit un salaire mensuel net de " + formData.salaireMensuel + " dirhams" +
+          civilite + " " + formData.nom.toUpperCase() + " " + formData.prenom.toUpperCase() + " perçoit un salaire mensuel net de " + formData.salaireMensuel + " dirhams" +
           " Suite à la demande de " + civilite + " " + formData.nom.toUpperCase() + " " + formData.prenom.toUpperCase() + ", employée auprès de notre établissement , nous nous engageons par la présente à virer irrévocablement et de manière permanente le salaire de " +
           civilite + " " + formData.nom.toUpperCase() + " " + formData.prenom.toUpperCase() + " , ainsi le cas échéant en cas de départ, le solde de tout compte, les indemnités de quelque soit la nature sur son compte ouvert auprès de la " +
           formData.banque + " sous les références:\n\n" + "RIB:\n\n";
           
           
-        const lines = doc.splitTextToSize(content, 170)
-        doc.text(lines, 20, 90)
+        const lines1 = doc.splitTextToSize(paragraph1, 170)
+        doc.text(lines1, 20, 70)
+        var yPosition = 160
+        doc.text("Code Banque  \n\n" + formData.codeBanque, 30, yPosition)
+        doc.text("Code Ville  \n\n" + formData.codeVille, 60, yPosition)
+        doc.text("Numéro de Compte  \n\n" + formData.NCompte, 90, yPosition)
+        doc.text("Clé RIB  \n\n" + formData.CleRib, 140, yPosition)
+        
+        doc.setLineWidth(0.5)
+        //trace les lignes horizontales
+        doc.line(27, 155, 170, 155) 
+        doc.line(27, 164, 170, 164) 
+        doc.line(27, 173, 170, 173) 
+        //trace les lignes verticales
+        doc.line(27, 155, 27, 173) 
+        doc.line(57, 155, 57, 173) 
+        doc.line(87, 155, 87, 173)
+        doc.line(137, 155, 137, 173)
+        doc.line(170, 155, 170, 173)
 
-        doc.text("Code Banque : " + formData.codeBanque, 20, 150)
-        doc.text("Code Ville : " + formData.codeVille, 20, 160)
-        doc.text("Numéro de Compte : " + formData.NCompte, 20, 170)
-        doc.text("Clé RIB : " + formData.CleRib, 20, 180)
-    
-        const yPosition = 180
-        doc.text("Fait à : Casablanca", 20, yPosition)
-        doc.text("Le : " + formatDateFrench(formData.dateDelivrance), 150, yPosition)
-        doc.text("Signature", 150, 200)
+        const paragraph2 = "En cas de départ de " + civilite + " " + formData.nom.toUpperCase() + " " + formData.prenom.toUpperCase() + ", nous nous engageons à informer la " + formData.banque.toUpperCase() + " dans les meilleurs délais et par tout moyen des montants de solde de tout compte , ainsi que les indemnités , de quelque soit la nature et procéder à leur virement au compte supra.\n" +
+          "Cet engagement restera valable jusqu'à notification écrite du BMCE , attestant le remboursement de toutes créances sur le compte supra."
+        const lines2 = doc.splitTextToSize(paragraph2, 170)
+        doc.text(lines2, 20, 190)
+
+        yPosition = 230
+        doc.text("Fait à : CASABLANCA", 70, yPosition)
+        doc.text("Le : " + formatDateFrench(formData.dateDelivrance), 115, yPosition)
+        doc.text("Signature de l'employeur", 90, yPosition+10)
+        doc.setFont("Times" , "bold")
+        doc.setTextColor(6, 176, 80)
+        doc.text("42 ALLEE DE MIMOSA ET ALLEE DES CITRONNIERS AIN SEBAA - Casablanca.", 30, 285)
+        doc.setTextColor(5, 11, 8)
+        doc.text("ICE: 003039695000032 IF: 52446555 – T.P: 32252457 - R.C: 543595 CNSS: 4878280", 35, 290)
 
         doc.save(`DOMICILIATION_IRRÉVOCABLE_DE_SALAIRE_${formData.nom}_${formData.prenom}.pdf`)
       }
@@ -149,11 +171,11 @@ function DomiciliationDeTravail() {
               <input type="date" name="dateDelivrance" value={formData.dateDelivrance} onChange={handleChange} required />
             </label>
           </div>
-          
+          <button type="submit">Générer le document</button>
   
           
         </form>
-        <button type="submit">Générer le document</button>
+        
       </div>
     )
 }
